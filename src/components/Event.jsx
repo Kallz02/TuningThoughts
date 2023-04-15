b64cde4e51fc3f0adfade7db5b8b71a06d48aa06
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-04-15 22:12:15 +0530

    remove redundant

---
+++ src/components/Event.jsx
@@ -0,0 +1,87 @@
+
+import Header from "./Header";
+
+import './Event.css'
+
+
+
+const Event = () => {
+    return (
+
+    <><Header />
+    <h1>Events</h1>
+    <p>
+    Direct Consultation available on our different branches all over India.
+1. Gudiyatham - Vellore
+2. Chennai
+3. Bangalore
+4. Salem
+5. Coimbatore
+Both online and offline Consultation available for 365 days dedicated for our
+people.
+Education and Registration
+Proper Education is provided for Healers with Guidelines of Alternative
+Medicine Association and registration is given for them for their practice after
+conducting exams.
+For Registration and other details, WhatsApp Office: +91 8838793089
+BENEFITS OF REGISTERING
+• Free Alternative medical camp
+• Guiding Higher Educations
+• Update informations about Alternative medicines
+• Consultations on 'How to Increase Your Practice'
+• Free Hosting, Programming and Web Page
+• We get benefits from W.H.O. / Central & State Government Polices.
+• Referral for Professional Liability Insurance.
+• Award and Honours.
+Members Rules & Regulation
+1. Any Alternative Medicine Practitioner can apply for Registration he / she
+must have experience & Qualification in the said field. He/ She can write (Hr.)
+Healer /Therapist in front of his/her name, but don't misguide any person
+(wrong practice is not allowed). You can advertise about your practice through
+Boards, pamphlets, newspapers. But you must mention your way of treatment
+in your Advertisement.
+2. The medical attendant (registered person) should avoid to take any serious
+case and if the situation arises that case (i.e. patient) must be
+transferred/admitted in hospital immediately.
+3. A person who has applied for registration must be working in his/her field
+honestly and sincerely.
+Alternative Medicine Practitioner cannot prescribe any of Ayurveda / Siddha/
+Homeopathic/ Unani /Allopathic medicine to any patient come for treatment.
+4. The alternative medicine practitioner means registered individual cannot
+sign and issue a death certificate. He/ She can issue only casual certificate and
+fees receipt.
+5. If in any case registered person he/she change Residential address/ Practice
+address/ Contact number requested to intimate about same to association in
+writing immediately.
+6. If registered person is found doing very good job for poor people in his or
+subject then may consider him/her for Best Practitioner Award.
+7. The ceremony venue will be communicated to all members by various
+means, like post/ Mobile Sms & email.
+8. If any registered person is found guilty due to misconduct or negligence
+under any or all like Indian Penal Code Act, State Medical Council Act, Indian
+Medical Council Act and declared guilty then never help to him/her at any cost.
+The registration of such individual will be terminated/ cancelled.
+9. The registered person can use RMAMP. The full form is “Register as a
+Member of Alternative Medicines Practitioner”. The registered person must
+mention RMAMP. Don’t misguide to any person come for treatment.
+10. The registered person will work honestly and sincerely. Please do not issue
+excess billing receipts false bills to patient.
+11. When you are honest sincere and correct in your duties still you face any
+genuine problem from any Medical Practitioner or Council in your locality, the
+association will extend you help and support but the legal expenditure has to
+be borne and paid by you.
+12. The association conducts health camp during which a registered individual
+can join in. The health camp may be organized by any other person anywhere
+in India & aboard.
+13. If any registered person wants to conduct a medical camp then he/she
+must mention the full name of association in his /her communications.
+14. This association if there is any change or modification made in rules and
+regulation we will send you revised copy of same & Email.
+15. The Registered person must adopt, abide and implement the rules and
+regulations of association during his p
+    </p>
+    </>
+    );
+  };
+  
+  export default Event;
