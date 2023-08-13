5e0f327c16c0a4dea3b939d05955e623d0c9c647
vak-01 <78302047+vak-01@users.noreply.github.com>
2023-08-13 12:21:00 +0530

    Completed the feedback page of the website

--- src/Pages/Feedback/Feedback.jsx
+++ src/Pages/Feedback/Feedback.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Feedbackgallery from '../../components/FeedbackGallery/Feedbackgallery';
 
 function Feedback(){
     return (
@@ -6,10 +7,8 @@ function Feedback(){
         <h1 className="carousel-heading text-[2.5rem] md:text-[4rem] my-6 font-light dark:text-gray-100">
                 <center>Feedback</center>
         </h1>
-        <p class="mt-4 text-xl text-gray-800 text-center  dark:text-gray-400">
-        உங்கள் கருத்துக்களை வழங்குவதன் மூலம் எங்கள் சேவைகளை மேம்படுத்த எங்களுக்கு உதவுங்கள்
-        </p>
-        {/* form contents to be added after further discussion */}
+        
+        <Feedbackgallery/>
               
         </>
     );
---
+++ src/components/FeedbackGallery/Feedbackgallery.jsx
@@ -0,0 +1,21 @@
+function Feedbackgallery() {
+    //public\feedback
+    const imagePaths = Array.from({ length: 13 }, (_, index) => `/feedback/photo${index + 1}.jpg`);
+    imagePaths.splice(4, 1);      // skips the 5th image
+  
+    return (
+        <div className="flex flex-wrap justify-center gap-10 mx-auto px-4 py-4">
+        {imagePaths.map((imagePath, index) => (
+            <div
+            key={index}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 p-4 bg-gray-200 rounded-md"
+            >
+            <img src={imagePath} alt={`Photo ${index + 1}`} className="w-full h-auto rounded-md" />
+            </div>
+        ))}
+        </div>
+    );
+  }
+  
+  export default Feedbackgallery;
+  
