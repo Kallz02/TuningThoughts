900a07ef8572d77549cf0a68a2850bc0cd897b95
Kallz02 <akshay.pranav.kalathil@gmail.com>
2023-08-16 01:00:20 +0530

    update Images and Homepage

--- src/components/Carousel/VideoCarousel.jsx
+++ src/components/Carousel/VideoCarousel.jsx
@@ -26,39 +26,40 @@ const VideoCarousel = ({ playlistId, api, title1 }) => {
     const videoUrl = `https://www.youtube.com/watch?v=${video.snippet.resourceId.videoId}`;
 
     return (
-      <div className="carousel-item w-[320px] mx-3" key={index}>
-        <Link
-          className="flex flex-col bg-white border shadow-sm rounded-lg hover:shadow-lg transition dark:bg-gray-900 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-          to={videoUrl}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            className="w-[320px] h-auto rounded-t-lg"
-            src={video.snippet.thumbnails.high.url}
-            alt={video.snippet.title}
-          />
-          <div className="p-2 md:p-4">
-            <h3 className="text-sm text-gray-800 font-normal dark:text-white">
-              {video.snippet.title}
-            </h3>
-          </div>
-        </Link>
-      </div>
+      video.snippet.thumbnails.high && (
+        <div className="mx-3 carousel-item w-[320px]" key={index}>
+          <Link
+            className="flex flex-col bg-white rounded-lg border shadow-sm transition dark:bg-gray-900 dark:border-gray-700 hover:shadow-lg dark:shadow-slate-700/[.7]"
+            to={videoUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="h-auto rounded-t-lg w-[320px]"
+              src={video.snippet.thumbnails.high.url}
+              alt={video.snippet.title}
+            />
+
+            <div className="p-2 md:p-4">
+              <h3 className="text-sm font-normal text-gray-800 dark:text-white">
+                {video.snippet.title}
+              </h3>
+            </div>
+          </Link>
+        </div>
+      )
     );
   };
 
   return (
     <>
-
-
-      <h1 className="carousel-heading text-3xl md:text-5xl my-5 dark:text-gray-100">
+      <h1 className="my-5 text-3xl md:text-5xl dark:text-gray-100 carousel-heading">
         <center>{title1}</center>
-        <p className="text-center text-sm">Swipe &#62;&#62;</p>
+        <p className="text-sm text-center">Swipe &#62;&#62;</p>
       </h1>
-      <div className="video-carousel-container mt-5 flex  overflow-x-auto  max-w-[95rem] mx-auto">
-        <div className="carousel-wrapper mt-5">
-          <div className="video-carousel flex ">
+      <div className="flex overflow-x-auto mx-auto mt-5 video-carousel-container max-w-[95rem]">
+        <div className="mt-5 carousel-wrapper">
+          <div className="flex video-carousel">
             {videos.map((video, index) => renderCarouselItem(video, index))}
           </div>
         </div>
