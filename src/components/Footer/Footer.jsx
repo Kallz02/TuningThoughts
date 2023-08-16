9ad5815d66a166f4a929fda3c466811ecd3593d1
vak-01 <78302047+vak-01@users.noreply.github.com>
2023-08-16 23:02:40 +0530

    added twitter link to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -134,6 +134,22 @@ function Footer() {
                         </Link>
                     </li>
 
+                    <li>
+                        <Link
+                            to="https://twitter.com/tuningthoughts?t=z6prgnVTvIFmuEEWkSEuUg&s=09"
+                            rel="noreferrer"
+                            target="_blank"
+                            class="text-gray-600 transition hover:text-gray-700/75"
+                        >
+                            <span class="sr-only">Twitter</span>
+
+                            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                            </svg>
+
+                        </Link>
+                    </li>
+
                 </ul>
 
             </div>
